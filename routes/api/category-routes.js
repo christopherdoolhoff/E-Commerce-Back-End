53719617d290636fc717649598869dec12da3d55
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-02-18 15:08:32 -0500

    added get one category route.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,22 +6,34 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   try {
-  const categoryData = await Category.findAll({
+  const categoriesData = await Category.findAll({
   // be sure to include its associated Products  
     include: [{model:Product}]
   });
-  res.status(200).json(categoryData);
+  res.status(200).json(categoriesData);
 } catch (err) {
   res.status(500).json(err);
 }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+    // be sure to include its associated Products
+    include: [{model: Product}],
+    });
+    if (!categoryData){
+      res.status(404).json({ message: 'No category found with that id!'});
+      return;
+    }
+    res.status(200).json(categoryData);
+} catch (err){
+  res.status(500).json(err);
+}
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
 });
 
