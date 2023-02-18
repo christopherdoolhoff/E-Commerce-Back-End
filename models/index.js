421692ac605d59d76513447ce0fe313064b85a52
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-02-18 14:19:15 -0500

    added Categories have many products to models/index.js.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsto(Category,{
+Product.belongsTo(Category,{
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
