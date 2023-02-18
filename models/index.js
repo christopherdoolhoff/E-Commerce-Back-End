9d0146c27a825aafd7561da6a80fadc423ca4f7c
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-02-18 14:15:05 -0500

    added product belongsTo Category in models/index.js.

--- models/index.js
+++ models/index.js
@@ -5,6 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsto(Category,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
 
