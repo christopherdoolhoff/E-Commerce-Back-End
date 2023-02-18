4ca903ca1e5ab6b4589c1ca4ef0eb257d0411c9a
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-02-18 15:22:30 -0500

    added post route for category.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   try {
   const categoriesData = await Category.findAll({
   // be sure to include its associated Products  
-    include: [{model:Product}]
+    include: [{model: Product}]
   });
   res.status(200).json(categoriesData);
 } catch (err) {
@@ -35,10 +35,19 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const createCategory = await Category.create(
+      req.body
+    );
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
